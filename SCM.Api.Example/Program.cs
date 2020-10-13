f804c58765b6288fa60d3137d59bf72cba396193
ting <tm731531@hotmail.com>
2020-10-13 16:25:24 +0800

    加入專案檔案。

---
+++ SCM.Api.Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCM.Api.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
